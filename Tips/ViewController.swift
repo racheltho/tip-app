5bed064637435a2fe51ab178233c42030b626254
Rachel Thomas <racheltho@gmail.com>
2015-01-03 12:51:48 +1100

    added bill split, updated constraints, changed color, updated screenshots

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -14,12 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitBillControl: UISegmentedControl!
+    @IBOutlet weak var perPersonLabel: UILabel!
+    @IBOutlet weak var perPersonField: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        perPersonField.hidden = true
+        perPersonLabel.hidden = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,12 +37,25 @@ class ViewController: UIViewController {
         var tipPercentages = [0.18, 0.2, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
+        var splitBillNums = [1, 2, 3, 4]
+        var splitBillNum = splitBillNums[splitBillControl.selectedSegmentIndex]
+        
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
+        var perPersonAmount = total / splitBillNum._bridgeToObjectiveC().doubleValue
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        if(splitBillNum > 1){
+            perPersonField.hidden = false
+            perPersonLabel.hidden = false
+            perPersonField.text = String(format: "$%.2f", perPersonAmount)
+        }else{
+            perPersonField.hidden = true
+            perPersonLabel.hidden = true
+        }
     }
 
     @IBAction func onTap(sender: AnyObject) {
