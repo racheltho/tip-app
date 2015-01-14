ed2fd1211a068737e5e86213b2c208d8aec888f1
Rachel Thomas <racheltho@gmail.com>
2015-01-14 10:49:31 -0800

    use NSNotificationCenter to set notification for AppDidEnterForeground and to display saved values or zero out fields, depending on how long it has been since Bill Amount was edited.

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -20,39 +20,64 @@ class ViewController: UIViewController {
     let defaults = NSUserDefaults.standardUserDefaults()
     let formatter = NSNumberFormatter()
     
+    func zeroOutFields(){
+        println("zeroing out")
+        formatter.numberStyle = .CurrencyStyle
+        if tipLabel != nil {
+            tipLabel.text = formatter.stringFromNumber(0)
+        }
+        if totalLabel != nil {
+            totalLabel.text = formatter.stringFromNumber(0)
+        }
+        if billField != nil {
+            billField.text = ""
+        }
+    }
+    
+    override func viewWillAppear(animated:Bool){
+        println("appearing")
+    }
+    
     override func viewDidLoad() {
+        println("loading")
         super.viewDidLoad()
         defaults.synchronize()
         perPersonField.hidden = true
         perPersonLabel.hidden = true
         tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_index")
-        formatter.numberStyle = .CurrencyStyle
-
-        // check if billAmount was entered in last 30 seconds
+        if defaults.objectForKey("billTimestamp") != nil {
+            var billAmount = defaults.doubleForKey("billAmount")
+            println(billAmount)
+            billField.text = NSString(format: "%.2f", billAmount)
+            tipLabel.text = defaults.objectForKey("tipLabelText") as? String
+            totalLabel.text = defaults.objectForKey("totalLabelText") as? String
+        } else {
+            zeroOutFields()
+        }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "willEnterForeground:", name: UIApplicationDidBecomeActiveNotification, object: nil)
+    }
+    
+    func willEnterForeground(notification: NSNotification!){
+        println("willEnterForeground")
+        // check if billAmount was entered in last 20 seconds
         var currentTime = NSDate()
         var lastChanged: NSDate!
         if defaults.objectForKey("billTimestamp") != nil {
-            
             lastChanged = defaults.objectForKey("billTimestamp") as NSDate
-            println(lastChanged)
             // time interval in seconds since bill Amount was entered
             let interval = currentTime.timeIntervalSinceDate(lastChanged)
-            if interval < 60 * 0.25 {
-                var billAmount = defaults.doubleForKey("billAmount")
-                println(billAmount)
-                billField.text = NSString(format: "%.2f", billAmount)
-                tipLabel.text = defaults.objectForKey("tipLabelText") as? String
-                totalLabel.text = defaults.objectForKey("totalLabelText") as? String
-            } else {
-                tipLabel.text = formatter.stringFromNumber(0)
-                totalLabel.text = formatter.stringFromNumber(0)
+            if interval > 20 {
+                zeroOutFields()
             }
-            
         }
         
-       
     }
     
+    deinit {
+        // make sure to remove the observer when this view controller is dismissed/deallocated
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: nil, object: nil)
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
